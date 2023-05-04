c7e38a6d9a6ef95349a3eac4212950f11823e8db
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2023-05-04 08:59:43 +0200

    login y registro

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', require('./routes/usuario'));
 
