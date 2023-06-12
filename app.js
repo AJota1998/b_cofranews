7eb9e2bed4b96da9f0683254d649feb6cd1482b5
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2023-06-12 16:12:45 +0200

    usando cors

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const bodyParser = require('body-parser');
 
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-})
+})*/
 
 
 app.use('/api', require('./routes/usuario'));
