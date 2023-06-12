f728db8fec96e971d02ffdb7e6420ec5ede3fe82
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2023-06-12 13:44:06 +0200

    rutas

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const cors = require('cors');
 
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 app.use('/api', require('./routes/usuario'));
 app.use('/api', require('./routes/colectivo'));
